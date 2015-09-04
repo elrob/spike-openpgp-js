7225e7901003f5533fda5350904edff2de49ec25
Rob Speller <rspeller@thoughtworks.com>
2015-09-04 16:33:34 +0100

    RS & ML | 155 | Take private key from a file input field and passphrase from an input field

--- decrypt.js
+++ decrypt.js
@@ -1,46 +1,30 @@
-var options = {
-    numBits: 2048,
-    userId: 'DUMMY <dummy@example.org>',
-    passphrase: 'password'
-};
+var decryptMessage = function(pgpMessageString, privkeyString, passphrase) {
 
-var startDecryptMessage = function(pgpMessageString, privkeyString) {
     var privateKeyObject = openpgp.key.readArmored(privkeyString).keys[0];
-    privateKeyObject.decrypt('password');
+    privateKeyObject.decrypt(passphrase);
 
-    console.log(pgpMessageString);
     var pgpMessageObject = openpgp.message.readArmored(pgpMessageString);
 
     openpgp.decryptMessage(privateKeyObject, pgpMessageObject).then(function(plaintext) {
         // success
-        console.log(plaintext);
+        document.getElementById('pgp-message').value = plaintext;
     }).catch(function(error) {
         // failure
         alert('decryption error');
     });
 };
 
-var startEncryptMessage = function(pubkeyString, privkeyString) {
-    var publicKeysObject = openpgp.key.readArmored(pubkeyString.toString());
-    openpgp.encryptMessage(publicKeysObject.keys, 'Hello, World!').then(function(pgpMessageString) {
-        // success
-        startDecryptMessage(pgpMessageString, privkeyString);
-    }).catch(function(error) {
-        // failure
-        alert('encryption error');
-    });
-};
+function readFileAndContinue(file, passphrase) {
+    reader = new FileReader();
+    reader.onload = function(e) {
+        var privateKey = e.target.result;
+        var pgpMessage = document.getElementById('pgp-message').value;
+        decryptMessage(pgpMessage, privateKey, passphrase);
+    };
+    reader.readAsText(file);
+}
 
-openpgp.generateKeyPair(options).then(function(keypair) {
-    // success
-    var pubkeyString = keypair.publicKeyArmored;
-    var privkeyString = keypair.privateKeyArmored;
-    startEncryptMessage(pubkeyString, privkeyString);
-}).catch(function(error) {
-    // failure
-    alert('error generating keypair');
-});
-
-var myAlert = function(){
-    alert(document.getElementsByName("passphrase")[0].value);
+var submitFunction = function(){
+    passphrase = document.getElementsByName("passphrase")[0].value;
+    privKey = readFileAndContinue(document.getElementById('private-key').files[0], passphrase);
 };
