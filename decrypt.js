dc9ca5ed169737a88c166c3cfdb58d0fec078441
Rob Speller <rspeller@thoughtworks.com>
2015-09-04 16:50:39 +0100

    RS & ML | 155 | Slight tidy-up

--- decrypt.js
+++ decrypt.js
@@ -1,30 +1,29 @@
-var decryptMessage = function(pgpMessageString, privkeyString, passphrase) {
+var pgpMessageElement = document.getElementById('pgp-message');
 
+var decryptMessage = function(pgpMessageString, privkeyString, passphrase) {
     var privateKeyObject = openpgp.key.readArmored(privkeyString).keys[0];
     privateKeyObject.decrypt(passphrase);
 
     var pgpMessageObject = openpgp.message.readArmored(pgpMessageString);
 
     openpgp.decryptMessage(privateKeyObject, pgpMessageObject).then(function(plaintext) {
-        // success
-        document.getElementById('pgp-message').value = plaintext;
+        pgpMessageElement.value = plaintext;
     }).catch(function(error) {
-        // failure
         alert('decryption error');
     });
 };
 
 function readFileAndContinue(file, passphrase) {
-    reader = new FileReader();
+    var reader = new FileReader();
     reader.onload = function(e) {
         var privateKey = e.target.result;
-        var pgpMessage = document.getElementById('pgp-message').value;
+        var pgpMessage = pgpMessageElement.value;
         decryptMessage(pgpMessage, privateKey, passphrase);
     };
     reader.readAsText(file);
 }
 
 var submitFunction = function(){
-    passphrase = document.getElementsByName("passphrase")[0].value;
-    privKey = readFileAndContinue(document.getElementById('private-key').files[0], passphrase);
+    var passphrase = document.getElementsByName("passphrase")[0].value;
+    readFileAndContinue(document.getElementById('private-key').files[0], passphrase);
 };
